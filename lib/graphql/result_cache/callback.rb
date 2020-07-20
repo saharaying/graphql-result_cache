dd89fed7f86ed2ad2a0f7af91ca2a785c1ca64d3
Robert Korzeniec <r.korzeniec@gmail.com>
2020-07-20 16:52:52 +0200

    allow ResultCache::Callback to use field object
    When using Field Extensions the context doens't store the path the same way, however the it has information about the field itself, so this can be passed on to the Callback class in order to provide correct logging.

--- lib/graphql/result_cache/callback.rb
+++ lib/graphql/result_cache/callback.rb
@@ -1,21 +1,32 @@
 module GraphQL
   module ResultCache
     class Callback
-      def initialize obj:, args:, ctx:, value:
+      def initialize(obj:, args:, ctx:, value:, field: nil)
         @obj = obj
         @args = args
         @ctx = ctx
         @value = value
+        @field = field
       end
 
-      def call result_hash
+      def call(result_hash)
         case @value
           when Symbol
             @obj.public_send(@value, result_hash)
           when Proc
             @value.call(result_hash, @obj, @args, @ctx)
         end
-        ::GraphQL::ResultCache.logger && ::GraphQL::ResultCache.logger.debug("GraphQL result cache callback called for #{@ctx.path.join('.')}")
+        ::GraphQL::ResultCache.logger && ::GraphQL::ResultCache.logger.debug("GraphQL result cache callback called for #{callback_caller}")
+      end
+
+      private
+
+      def callback_caller
+        ctx_path || @field.name
+      end
+
+      def ctx_path
+        @ctx.path.empty? ? nil : @ctx.path.join('.')
       end
     end
   end
